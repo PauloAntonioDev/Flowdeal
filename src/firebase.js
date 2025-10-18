126874f913e10dbd2d6cc85175912eb371939b5d
Paulo Antonio Muñoz Martinez <paulomunozmartinez86@gmail.com>
2025-10-18 09:10:59 -0700

    -

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+const requiredEnv = [
+  'VITE_FIREBASE_API_KEY',
+  'VITE_FIREBASE_AUTH_DOMAIN',
+  'VITE_FIREBASE_PROJECT_ID',
+  'VITE_FIREBASE_STORAGE_BUCKET',
+  'VITE_FIREBASE_MESSAGING_SENDER_ID',
+  'VITE_FIREBASE_APP_ID'
+];
+const missingEnv = requiredEnv.filter((k) => !import.meta.env[k]);
+if (missingEnv.length) {
+  console.error('Faltan variables de entorno de Firebase:', missingEnv.join(', '));
+  throw new Error('Config de Firebase incompleta. Define las variables de entorno.');
+}
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
